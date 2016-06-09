1c802a80f988b1ddc4b616a12128a6829e8db486
Andrey <andrey_ivanov_sub@mail.ru>
2016-06-09 19:10:47 +0300

    commit1

---
+++ lesson 3/script.js
@@ -0,0 +1,35 @@
+/*
+function animals(heads, legs){
+	for ( var i = 0; i <= heads; i++) {
+    var Chickens =(4 * heads - legs) / 2;
+};
+	for ( var i = 0; i <= heads; i++) {
+    var Cows = heads - Chickens;
+};
+    return [Chickens, Cows];
+}
+    console.log(animals(20,60));
+*/
+
+
+/*function howManyAnimals(number){
+	var number = [1,2,3,4,5];
+	number[100]= 101;
+	var reaction = ["Чего так мало?", "Более чем достаточно", "Почему их так много", "Ого, 101 далматинец!"];
+	for ( var i =0; i <=10; i++) {
+	var respond = reaction[0];	
+	}
+    for ( var i =0; i <=50; i++) {
+	var respond = reaction[1];	
+	}
+	for ( var i =0; i <=100; i++) {
+	var respond = reaction[2];	
+	}
+	for ( var i=0;i == 101; i++) {
+	var respond = reaction[3];
+	}	
+	return respond
+};
+    
+    console.log(howManyAnimals(16));*/
+
