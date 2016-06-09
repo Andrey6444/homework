71dc5218f064a0d9fd60f5b2f2ac2592d770e6e0
Andrey <andrey_ivanov_sub@mail.ru>
2016-06-09 21:14:50 +0300

    commit2

--- lesson 3/script.js
+++ lesson 3/script.js
@@ -1,3 +1,5 @@
+// exercise 1
+
 /*
 function animals(heads, legs){
 	for ( var i = 0; i <= heads; i++) {
@@ -11,6 +13,7 @@ function animals(heads, legs){
     console.log(animals(20,60));
 */
 
+// exercise 2
 
 /*function howManyAnimals(number){
 	var number = [1,2,3,4,5];
@@ -32,4 +35,19 @@ function animals(heads, legs){
 };
     
     console.log(howManyAnimals(16));*/
+	
+// exercise 3
+
+	/*var num = 1;
+for ( ; num < 101; num++ ) {
+    if ( num % 5 === 0 && num % 3 === 0 ){
+      console.log( num,  "= FizzBuzz" )
+    }else if ( num%5 === 0 ){
+       console.log( num, " = Buzz" );
+    }else if ( num%3 === 0 ){
+       console.log( num, " = Fizz" );
+    }else{
+       console.log( num );
+    }
+}*/
 
